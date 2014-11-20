2bf1b5b0945e39284a51cdafad410f89f13e075e
lincomatic <lincomatic@hotmail.com>
2014-11-20 14:39:22 -0800

    change version

--- open_evse.h
+++ open_evse.h
@@ -35,7 +35,7 @@
 #include "WProgram.h" // shouldn't need this but arduino sometimes messes up and puts inside an #ifdef
 #endif // ARDUINO
 
-#define VERSION "D3.2.9"
+#define VERSION "D3.3.0"
 
 //-- begin features
 
