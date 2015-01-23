42947f033892359028c9251ac914fde051c3d463
lincomatic <lincomatic@hotmail.com>
2015-01-22 21:23:08 -0800

    update for pull requests 19,20

--- open_evse.h
+++ open_evse.h
@@ -35,7 +35,7 @@
 #include "WProgram.h" // shouldn't need this but arduino sometimes messes up and puts inside an #ifdef
 #endif // ARDUINO
 
-#define VERSION "D3.3.6"
+#define VERSION "D3.3.7"
 
 //-- begin features
 
