6850b24b4e334ef2bd68323ebb80dbc4366f7c21
lincomatic <lincomatic@hotmail.com>
2015-05-12 22:07:12 -0700

    v3.7.8 release

--- open_evse.h
+++ open_evse.h
@@ -36,7 +36,7 @@
 #include "WProgram.h" // shouldn't need this but arduino sometimes messes up and puts inside an #ifdef
 #endif // ARDUINO
 
-#define VERSION "D3.7.8"
+#define VERSION "3.7.8"
 
 //-- begin features
 
