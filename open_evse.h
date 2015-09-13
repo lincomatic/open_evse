b1b28635bb309894917853a60ccd4d3ddb6ad6c2
Craig Kirkpatrick <craigk46@comcast.net>
2015-09-12 17:35:07 -0700

    Fixing Temperature Monitoring broken since D3.9.5

--- open_evse.h
+++ open_evse.h
@@ -36,7 +36,7 @@
 #include "WProgram.h" // shouldn't need this but arduino sometimes messes up and puts inside an #ifdef
 #endif // ARDUINO
 
-#define VERSION "D3.9.7"
+#define VERSION "D3.9.8"
 
 //-- begin features
 
