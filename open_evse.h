2eeb234dd993da347953282bb77446112624726b
lincomatic <lincomatic@hotmail.com>
2015-09-14 21:19:54 -0700

    D3.9.9 version tag

--- open_evse.h
+++ open_evse.h
@@ -36,7 +36,7 @@
 #include "WProgram.h" // shouldn't need this but arduino sometimes messes up and puts inside an #ifdef
 #endif // ARDUINO
 
-#define VERSION "D3.9.8"
+#define VERSION "D3.9.9"
 
 //-- begin features
 
