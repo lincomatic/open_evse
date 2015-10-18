58bad11209898cc913f729c4b7af78b381e32938
William McBrine <wmcbrine@gmail.com>
2015-10-18 00:09:52 -0400

    MakeChar() -- 22 bytes saved.

--- open_evse.h
+++ open_evse.h
@@ -643,6 +643,7 @@ class OnboardDisplay
 
   int8_t updateDisabled() { return  m_bFlags & OBDF_UPDATE_DISABLED; }
 
+  void MakeChar(uint8_t n, const char PROGMEM *bytes);
 public:
   OnboardDisplay();
   void Init();
