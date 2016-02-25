12edacee3067eb6abf2f7d6020e8aa358d6786c5
lincomatic <lincomatic@hotmail.com>
2016-02-25 15:30:59 -0800

    update version

--- open_evse.h
+++ open_evse.h
@@ -38,7 +38,7 @@
 #include "WProgram.h" // shouldn't need this but arduino sometimes messes up and puts inside an #ifdef
 #endif // ARDUINO
 
-#define VERSION "D4.1.0"
+#define VERSION "D4.1.1"
 
 #include "Language_default.h"   //Default language should always be included as bottom layer
 
