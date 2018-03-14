7b02f1ef8971a75be8735445af3223de4310b38e
scl <lincomatic@gmail.com>
2018-03-13 17:51:49 -0700

    THREEPHASE cleanup

--- firmware/open_evse/EnergyMeter.cpp
+++ firmware/open_evse/EnergyMeter.cpp
@@ -61,11 +61,12 @@ void EnergyMeter::calcUsage()
       uint32_t ma = g_EvseController.GetChargingCurrent();
 #ifdef VOLTMETER
       m_wattSeconds += ((g_EvseController.GetVoltage()/1000UL) * (ma/1000UL) * dms) / 1000UL;
-#endif
+#else // !VOLTMETER
 #ifdef THREEPHASE //Multiple L1 current by the square root of 3 to get 3-phase energy
       m_wattSeconds += (((g_EvseController.GetCurSvcLevel() == 2) ? VOLTS_FOR_L2:VOLTS_FOR_L1) * (ma/1000UL) * dms * 3) / 1000UL;
-#else // !VOLTMETER
+#else // !THREEPHASE
      m_wattSeconds += (((g_EvseController.GetCurSvcLevel() == 2) ? VOLTS_FOR_L2:VOLTS_FOR_L1) * (ma/1000UL) * dms) / 1000UL;
+#endif // THREEPHASE
 #endif // VOLTMETER
 
       m_lastUpdateMs = curms;
