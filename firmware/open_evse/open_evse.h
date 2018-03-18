d37aa257c74c972039527794134bff408372eaad
Glyn Hudson <glyn.hudson@gmail.com>
2018-03-18 10:24:15 +0000

    bump version 4.12.3.EU1

--- firmware/open_evse/open_evse.h
+++ firmware/open_evse/open_evse.h
@@ -41,7 +41,7 @@
 #define setBits(flags,bits) (flags |= (bits))
 #define clrBits(flags,bits) (flags &= ~(bits))
 
-#define VERSION "4.12.3.EU"
+#define VERSION "4.12.3.EU1"
 
 
 #include "Language_default.h"   //Default language should always be included as bottom layer
