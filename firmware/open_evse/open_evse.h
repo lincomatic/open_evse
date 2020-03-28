b0249faeecb62e311151ee1865ff9b06e2f71b3f
scl <lincomatic@gmail.com>
2020-03-27 18:18:59 -0700

    D6.2.0

--- firmware/open_evse/open_evse.h
+++ firmware/open_evse/open_evse.h
@@ -42,7 +42,7 @@
 #define clrBits(flags,bits) (flags &= ~(bits))
 
 #ifndef VERSION
-#define VERSION "D6.1.3"
+#define VERSION "D6.2.0"
 #endif // !VERSION
 
 #include "Language_default.h"   //Default language should always be included as bottom layer
